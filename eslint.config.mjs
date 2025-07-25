d8e679f6e3d702c72ccaaea5befd29760d0e9ee4
aman verma <aman.verma@reynasolutions.onmicrosoft.com>
2025-07-25 16:46:30 +0530

    Setup done

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = {
+  ...projectCompilationEventsSubscribe.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'plugin:react/recommended'],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+    },
+  })
+}
+
+export default eslintConfig;
